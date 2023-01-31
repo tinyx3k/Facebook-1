ebc15b6a2f763a881d76bd3b8ceaa3f8ef7160d9
Ferly Shishigami-X <122451961+Shishigami-X@users.noreply.github.com>
2023-01-31 20:14:35 +0700

    Update Run.py

--- Run.py
+++ Run.py
@@ -83,11 +83,11 @@ def logo(n):
 {U} |    \ |  _  || __  ||  |  |  {U} |   __||  _  ||     ||   __|| __  ||     ||     ||  |  |
 {K} |  |  ||     ||    -||    -|  {K} |   __||     ||   --||   __|| __ -||  |  ||  |  ||    -|
 {P} |____/ |__|__||__|__||__|__|  {P} |__|   |__|__||_____||_____||_____||_____||_____||__|__|
-			{M}•{K}•{H}• {P}MULTI BRUTE FORCE FACEBOOK {H}•{K}•{M}•
-                        {M}•{K}•{H}• {P} By Author   : Ferly Shishigami         
-                        {M}•{K}•{H}• {P}My Github   : github.com/Shishigami-X  
-			{M}•{K}•{H}• {P}My Hp/Wa    : +6285797818026           
-			{M}•{K}•{H}• {P}My Facebook : フェリー シシガ""")                
+     {M}•{K}•{H}• {P}MULTI BRUTE FORCE FACEBOOK {H}•{K}•{M}•
+     {M}•{K}•{H}• {P} By Author   : Ferly Shishigami {H}•{K}•{M}•       
+     {M}•{K}•{H}• {P}My Github   : github.com/Shishigami-X {H}•{K}•{M}•
+     {M}•{K}•{H}• {P}My Hp/Wa    : +6285797818026 {H}•{K}•{M}•          
+     {M}•{K}•{H}• {P}My Facebook : フェリー シシガ {H}•{K}•{M}•""")                
 def logo2():
 	return str(f"""
 {M}  ____   _____  _____  _____   {M}  _____  _____  _____  _____  _____  _____  _____  _____
